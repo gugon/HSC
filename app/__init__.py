1944def755997078083c4641bade1ff231dcc738
gugon <gugon19@gmail.com>
2023-11-10 18:34:13 -0300

    Cadastro de login/Login no sistema/logout

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,34 @@
 from flask import Flask
 from dynaconf import FlaskDynaconf
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
 
 from app.nav import create_nav
+# from app.models.models import create_db
+# from app.models.models import db
 
 app = Flask(__name__)
 FlaskDynaconf(app)
 Bootstrap(app)
+
+# db = SQLAlchemy(app)
+# db.init_app(app)
+# create_db(app)
+db = SQLAlchemy(app)
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
+
+medico = Base.classes.Medico
+paciente = Base.classes.Paciente
+imagem = Base.classes.Imagem
+exame = Base.classes.Exame
+# visitantes = Base.classes.Visitante
+endereco = Base.classes.Endereco
+pacHasMed = Base.classes.Paciente_has_Medico
+compartilhado = Base.classes.Compartilhado
+
+
 create_nav(app)
 
 from app.controllers import default
--- app/nav.py
+++ app/nav.py
@@ -4,30 +4,36 @@ from flask_nav.elements import Navbar, View, Subgroup
 
 def create_nav(app):
     nav = Nav()
+    nav.init_app(app)
 
     @nav.navigation()
     def menunavbar():
         menu = Navbar('HSC')
-        menu.items = [View('Home', 'teste')]
-        menu.items.append(Subgroup('Cadastros',
-                            View('Exames', 'teste'),
-                                   # View('Informações Gerais', 'cadastroInfoGeral'),
-                                   View('Meus Dados', 'teste'),
-                                   View('Meus Médicos', 'teste')))
+        menu.items = [View('Home', 'teste'),
+                      Subgroup(
+                          'Cadastros',
+                          View('Meus Dados', 'autenticar'),
+                          View('Exames', 'cadastroRapido'),
+                          View('Meus Médicos', 'teste')
+                          # Link('Tech Support', 'https://google.com')
+                      ),
+                      Subgroup(
+                          'Visualizar Exames',
+                          View('RX', 'teste'),
+                          View('Ressonância Magnética', 'teste'),
+                          # Link('Tech Support', 'https://google.com')
+                      )
+                      ]
 
-        menu.items.append(Subgroup('Visualizar Exames',
-                            View('RX', 'teste'),
-                                   View('Ressonância Magnética', 'teste'),
-                                   View('Ultrassom', 'teste'),
-                                   View('Teste', 'teste'),
-                                   View('Lista', 'teste')))
-
-        # menu.items.append(Subgroup('Compartilhar Exames',
-        #                            View('RX', 'testes2'),
-        #                            View('Ressonância Magnética', 'resonancia'),
-        #                            View('Ultrassom', 'mostraLaudo')))
-
-        menu.items.append(View('Sair', 'teste'))
+        menu.items.append(View('Sair', 'logout'))
         return menu
 
-    nav.init_app(app)
+    @nav.navigation()
+    def menumedico():
+        menu = Navbar('Medico')
+        menu.items = [View('Home', 'teste'),
+                      View('Visualizar Exames', 'teste')
+                      ]
+        menu.items.append(View('Sair', 'logout'))
+        # menu.items.append(Subgroup('Visualizar Exames', View('RX', 'gerenciaRx')))
+        return menu
